51544ebf65510d1c3272426976fc9d7c396af84e
Stanislav_Lushnikov <lust48h@gmail.com>
2023-02-03 23:02:40 +0300

    add homework task019

---
+++ Task019/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length != 5)
+{
+    Console.WriteLine("Введено неверное число");
+    Console.WriteLine("Введите число от 10000 до 99999");
+}
+
+int halfLenNumber = number.Length / 2;
+int tailInd = number.Length - 1;
+string result = "да";
+for (int frontInd = 0; frontInd <= halfLenNumber; frontInd++)
+{
+    if (number[frontInd] == number[tailInd]) tailInd--;
+    else
+    {
+        result = "нет";
+        break;
+    }
+}
+Console.WriteLine($"{number} -> {result}");
